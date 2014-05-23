7842a89f0ced8685a4f2ed0404991ab79eccd7da
Nicolas Braem <nicolas.braem@gmail.com>
2014-05-23 14:57:42 +0200

    Update dotinstall.sh

--- dotinstall.sh
+++ dotinstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo gem install homesick
-homesick clone nbraem/dotfiles && homesick symlink dotfiles
+homesick clone nbraem/dotfiles && homesick symlink -f dotfiles
 homesick clone nbraem/dotvim && homesick symlink dotvim
