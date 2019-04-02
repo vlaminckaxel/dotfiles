0346463973cc43d35f84451991ee1eefa10ecec5
axel.vlaminck <axel.vlaminck@gmail.com>
2019-04-02 13:59:05 +0200

    Update dotinstall.sh

--- dotinstall.sh
+++ dotinstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt-get install -y ruby
 sudo gem install homesick 
-homesick clone nbraem/dotfiles && homesick symlink dotfiles --force
-homesick clone nbraem/dotvim && homesick symlink dotvim
+homesick clone vlaminckaxel/dotfiles && homesick symlink dotfiles --force
+homesick clone vlaminckaxel/dotvim && homesick symlink dotvim
 source ~/.bashrc
 sudo apt-get install -y exuberant-ctags silversearcher-ag software-properties-common
 
